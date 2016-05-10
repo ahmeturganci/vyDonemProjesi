c6b7d954312b80f4513a36d15cb6704b75fe3324
ahmeturganci <aahmet.272@gmail.com>
2016-05-10 10:40:23 +0300

    Başvuru yapan interface eklendi

---
+++ vyDonemProjesi/vyDonemProjesi/IBasvuruYapan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vyDonemProjesi
+{
+   public  interface IBasvuruYapan
+    {
+       Kisi Kaydol(); // sisiteme kisi bilgilerini girecek
+       Kisi BilgiGüncelleme();
+       void SistemdenCikis();
+       object BasvuruYap();
+
+
+
+
+    }
+}
