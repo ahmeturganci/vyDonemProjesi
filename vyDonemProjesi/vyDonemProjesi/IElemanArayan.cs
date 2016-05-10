8040c27e54e2cdb2573fc97b9ae598d2d3a2248e
ahmeturganci <aahmet.272@gmail.com>
2016-05-10 10:55:47 +0300

    .

---
+++ vyDonemProjesi/vyDonemProjesi/IElemanArayan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vyDonemProjesi
+{
+    public interface IElemanArayan
+    {
+        Eleman Kayit();
+        void Guncelle();
+        void IlanVe();
+        void AdayListele();
+        void EnUygunuIseAl();//heapten öncelikli kuyrukla çekilecek
+            
+
+    }
+}
