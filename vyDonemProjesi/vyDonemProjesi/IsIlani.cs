548ed1f16362b974921b0ec00ffcdc6c962cb173
mehmetgelmedi <mehmetemingelmedi@gmail.com>
2016-05-11 15:00:38 +0300

    .

---
+++ vyDonemProjesi/vyDonemProjesi/IsIlani.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vyDonemProjesi
+{
+    public class IsIlani
+    {
+        public string isTanimi { get; set; }
+        public string arananOzellikler { get; set; }
+    }
+}
--- vyDonemProjesi/vyDonemProjesi/Sirket.cs
+++ vyDonemProjesi/vyDonemProjesi/Sirket.cs
@@ -7,12 +7,12 @@ namespace vyDonemProjesi
 {
    public  class Sirket
     {
-        public string isyeri { get; set; }
+       public IsYeri isyeri { get; set; }
+       public IsIlani isIlani { get; set; }
+       public Eleman ArananEleman { get; set; }
         public string tamAdres { get; set; }
         public string Telefon { get; set; }
         public string Faks { get; set; }
         public string EPosta { get; set; }
-
-        public Eleman ArananEleman { get; set; }
     }
 }
